0c4c59c2e999461f0b238ab7625145036ee8f9b6
wei840220 <wei840220@gmail.com>
2017-09-16 01:55:46 +0800

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,87 +1,103 @@
-var dataurl = "data.json";
+;(function($){
+  'use strict';
 
-var rlist = [];
-var list_html = "<li id={id}> <name>{name}</name><button class='del' id={id}>×</button></li>";
+  // global
+  var rlist = [];
+  var first_load = true;
+  var time = undefined;
+  var list_html = "<li id={id}> <name>{name}</name><button class='del' id={id}>×</button></li>";
 
-$.ajax({
-  url: dataurl,
-  dataType: "json",
-  error: function(){
-    console.log("error");
-  },
-  success: function(res){
-    console.log("success");
-    rlist = res;
-    update();
+  var _init = function(){
+    _initLoad();
+    _initEventListeners();
+  };
+
+  var _initLoad = function(){
+    var dataurl = "data.json";
+
+    $.ajax({
+      url: dataurl,
+      dataType: "json",
+      error: function(){
+        console.log("error");
+      },
+      success: function(res){
+        console.log("success");
+        rlist = res;
+        _update();
+      }
+    });
   }
-});
 
-var first_load = true;
-function update(){
-  $("ul").html("");
-  for (var i=0; i<rlist.length; i++){
-    // use g glag to replace globally
-    var current_list_html = 
-        list_html.replace(/{id}/g, i) 
-                 .replace("{name}", rlist[i].name);
-    if (!first_load){
-      $("ul").append(current_list_html);  
+  var _update = function (){
+    $("ul").html("");
+    for (var i = 0; i < rlist.length; i++){
+      var current_list_html = 
+          list_html.replace(/{id}/g, i) 
+                   .replace("{name}", rlist[i].name);
+      if (!first_load)
+        $("ul").append(current_list_html);  
+      else
+        $(current_list_html).hide().appendTo("ul").delay( i*100 ).fadeIn(1000);
     }
-    else{
-      $(current_list_html).hide().appendTo("ul").delay( i*100 ).fadeIn(1000);
-    }
+    first_load = false;
+    
+    $("button.del").click( function(){
+      rlist.splice(this.id,1);
+      _update();
+    });
   }
-  first_load = false;
+
+  var _initEventListeners = function(){
+    $(".add_btn").click( function(){
+      if ( $("input").val()=="" ){
+        alert("Invalid input. Please enter again.");
+      }
+      else {
+        rlist.push({ name:$("input").val() });
+        $("input").val("");
+        _update();
+      }
+    });
+    
+    var open = false;
+    $(".pick_btn").click( function(){
+        if (!open){
+          $(".fill_stock").addClass("fill_stock_open");
+          open = true;
+        }
+       
+        if (rlist.length == 0)  {
+          $("h2").html("");
+          $("h2").append("<i class='fa fa-cutlery' aria-hidden='true'></i><h4>Oops! Haven't add any restaurants?</h4>");
+        }
+        else  {
+          countdown();
+        }
+    });
+  };
   
-  $("button.del").click( function(){
-    rlist.splice(this.id,1);
-    update();
-  });
-}
+  var pick = function(){
+    $("h2").html("");
+    var num = Math.floor( Math.random()*(rlist.length) );
+    $("h2").append(rlist[num].name);
+  };
+  
+  var countdown = function(){
+    time = 15;
+    var intervalID = window.setInterval( function(){
+      if (time>0){
+        time--;
+        pick();
+      }
+      else {
+        clearInterval(intervalID);
+      }
+    }, 100);
+  }  
 
-$(".add_btn").click( function(){
-  if ( $("input").val()=="" ){
-    alert("Invalid input. Please enter again.");
-  }
-  else {
-    rlist.push({ name:$("input").val() });
-    $("input").val("");
-    update();
-  }
+  $(document).ready(function(){
+    _init();
 });
 
-var open = false;
-$(".pick_btn").click( function(){
-    if (!open){
-      $(".fill_stock").addClass("fill_stock_open");
-      open = true;
-    }
-   
-    if (rlist.length == 0)  {
-      $("h2").html("");
-      $("h2").append("<i class='fa fa-cutlery' aria-hidden='true'></i><h4>Oops! Haven't add any restaurants?</h4>");
-    }
-    else  {
-      countdown();
-    }
-});
-
-function pick(){
-  $("h2").html("");
-  var num = Math.floor( Math.random()*(rlist.length) );
-  $("h2").append(rlist[num].name);
-}
-
-var time;
-function countdown(){
-  time = 15;
-  intervalID = window.setInterval( function(){
-    if (time>0){
-      time--;
-      pick();
-    }
-    else {
-      clearInterval(intervalID);
-    }
-  }, 100);
-}
+})(jQuery);
